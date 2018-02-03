dbb649d6aab24602bddd6a86cfc864d75309d5d7
DIR\j.velasco.feria <j.velasco.feria@accenture.com>
2018-02-03 15:47:59 +0100

    Añado una línea

--- src/main/java/es/joa/git/App.java
+++ src/main/java/es/joa/git/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Esto es un ejemplo de programa Java" );
+        System.out.println( "Esto es un ejemplo de programa Java" );
     }
 }
