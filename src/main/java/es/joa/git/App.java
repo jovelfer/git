d3471d3f1592719b4229b85f76b8e71f93ac5bf7
DIR\j.velasco.feria <j.velasco.feria@accenture.com>
2018-02-03 17:01:12 +0100

    Añadido método 1 en Feature2

--- src/main/java/es/joa/git/App.java
+++ src/main/java/es/joa/git/App.java
@@ -9,6 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Esto es un ejemplo de programa Java" );
-        System.out.println( "Esto es un ejemplo de programa Java" );
     }
 }
--- src/main/java/es/joa/git/Feature2.java
+++ src/main/java/es/joa/git/Feature2.java
@@ -1,5 +1,7 @@
 package es.joa.git;
 
 public class Feature2 {
-
+	public void metodo1(){
+		System.out.println( "Método 1 de Feature2" );
+	}
 }
