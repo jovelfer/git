55452923f1f7f7a798a9817b310e6c34b92a9da5
DIR\j.velasco.feria <j.velasco.feria@accenture.com>
2018-02-04 17:52:52 +0100

    Método 2 de Feature2

--- src/main/java/es/joa/git/Feature2.java
+++ src/main/java/es/joa/git/Feature2.java
@@ -4,4 +4,9 @@ public class Feature2 {
 	public void metodo1(){
 		System.out.println( "Método 1 de Feature2" );
 	}
+	
+	public void metodo2(){
+		System.out.println( "Método 2 de Feature2" );
+	}
+	
 }
